3accda37e9ade7fb30af037e0259029c7337f46e
Liz Giraldo <lizgiraldo@utp.edu.co>
2025-09-15 12:02:46 -0500

    WIP: Ajuste de scripts en registro y data usada

--- cypress/e2e/registro_ticketazo.cy.js
+++ cypress/e2e/registro_ticketazo.cy.js
@@ -1,220 +1,198 @@
 import RegistroPage from '../support/pages/RegistroPage';
+import {generarCorreo, generarDNI} from '../support/utils';  // para validar data dinámica 
 
 describe('Pruebas de Registro en Ticketazo', () => {
   const registroPage = new RegistroPage();
+  let data;
 
   beforeEach(() => {
     cy.visit('https://ticketazo.com.ar/auth/registerUser'); // URL de prueba
+    cy.fixture('userRegister').then((fData) =>{
+      data=fData;
+    });
   });
 
   // Happy path
-  it('Registro exitoso con datos válidos', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.usuarioValido;
+  it('Registro exitoso de usuario nuevo', () => {
+  // cargo la data de usuario válido fijo que esta en fixture
+  cy.fixture('userRegister').then((data) => {
+    const baseUser = data.usuarioValido;
 
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
+    // sobrescribo lo que necesito aleatorio
+    const userConDatosAleatorios = {
+      ...baseUser,
+      email: generarCorreo(),
+      confirmarEmail: generarCorreo(),
+      dni: generarDNI()
+    };
 
-      cy.get(registroPage.buttonEnviar).click();
+    // uso el método para llenar los datos generales 
+    registroPage.completarDatosGenerales(userConDatosAleatorios);
 
-      
+    // interceptar la petición de registro
+    cy.intercept('POST','**/api/backend/register/register-user').as('register');
+
+    cy.get(registroPage.buttonEnviar).click();
+
+    // validar que el usuario si se registre exitosamente consultando la respuesta de la petición de registro
+    cy.wait('@register').then((interception) => {
+      expect(interception.response.statusCode).to.eq(201);
     });
   });
+});
+
+
 
   // Verificar campos vacíos y mensaje de error 
-  it('Validación de errores cuando los campos están vacíos', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.usuarioSinDatos;
-
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
-
-      cy.get(registroPage.buttonEnviar).click();
-
-      // método  para evaluar el mensaje de error de completa este campo
+  it('Validar mensaje de error por campos requeridos vacío', () => {
+    registroPage.completarDatosGenerales(data.usuarioSinDatos);
+    cy.get(registroPage.buttonEnviar).click();
+    // método  para evaluar el mensaje de error de completa este campo
       registroPage.validarError();
     });
-  });
-
-
+ 
 
 // Verificar campos que no cumplen con el formato de la cantidad de caracteres permitidos 
 //o del formato admitido para el campo como el correo.
   it('Validación de formato de datos', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.usuarioDatosNoTienenFormato;
-      
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.validarErrorFormato(registroPage.inputTelefono, user.telefono);
-      registroPage.validarErrorFormato(registroPage.inputDni, user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.validarErrorEmail(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
+    registroPage.completarDatosGenerales(data.usuarioDatosNoTienenFormato);
+    registroPage.validarErrorFormato(registroPage.inputTelefono, data.usuarioDatosNoTienenFormato.telefono);
+    registroPage.validarErrorFormato(registroPage.inputDni, data.usuarioDatosNoTienenFormato.dni);
+    registroPage.validarErrorEmail(registroPage.inputCorreo, data.usuarioDatosNoTienenFormato.email);
+    cy.get(registroPage.buttonEnviar).click();    
+  });
 
+
+
+// Verificar mensaje de error cuando ingreso un correo electrónico invalido sin completar si es .es .com .net
+
+it('Validar mensaje de error al ingresar un correo electrónico inválido', () => {
+      registroPage.completarDatosGenerales(data.usuarioCorreInvalido);
       cy.get(registroPage.buttonEnviar).click();
+      // método  para evaluar el mensaje de error de correo inválido
+      registroPage.validarDatoInvalido();
+    
+  });
+  
 
-      
-  });
-  });
 
   // Verificar que los correos sean iguales una vez se complete el registro
-   it('Validación de consistencia en correos ingresados', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.usuarioDiferenteCorreo;
-      
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
-
+   it('Validar coincidencia entre el correo y su confirmación en el registro', () => {
+      registroPage.completarDatosGenerales(data.usuarioDiferenteCorreo);
       cy.get(registroPage.buttonEnviar).click();
       // método  para evaluar el mensaje de error de no coincidencia en los correos ingresados
       registroPage.validarCoincidenciaEmail();
-
-      
-  });
   });
   
+  
   // Verificar que las contraseñas sean iguales una vez se complete el registro
-   it('Validación de consistencia en contraseñas ingresadas', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.usuarioDiferentePassword;
-      
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
-
+   it('Validar coincidencia entre la contraseña ingresada y su confirmación en el registro. ', () => {
+     registroPage.completarDatosGenerales(data.usuarioDiferentePassword);
       cy.get(registroPage.buttonEnviar).click();
+
+      
       // método  para evaluar el mensaje de error de no coincidencia en las contraseñas ingresadas
       registroPage.validarCoincidenciaPassword();
+  });
 
-      
-  });
-  });
 
 // Verificar que las contraseñas cumplan con la cantidad de caracteres solicitada
-   it('Validación de contraseña con caracteres solicitados', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.contraseñaSinFormato;
-      
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
+   it('Validar longitud mínima y criterios de complejidad de contraseña en registro', () => {
+      registroPage.completarDatosSinPassword(data.contraseñaSinFormato);
 
-      cy.get(registroPage.buttonEnviar).click();
-      // método  para evaluar el mensaje de error de contraseña no válida
-      registroPage.validarFormatoPassword();
+      const invalidPasswords = ['ab1@defg', 'Abcdef@#', 'Abcdef12']
+      cy.wrap(invalidPasswords).each((pass) => {
+      // escribir/limpiar usando los métodos del page object
+      registroPage.escribirPassword(pass)          
+      registroPage.confirmarPassword(pass)  
+       // interceptar la petición de registro
+       cy.intercept('POST','**/api/backend/register/register-user').as('register')       
+       cy.get(registroPage.buttonEnviar).click();
+
+       cy.wait('@register').then((interception) => {
+      // asegurarse que no sea un 201 (usuario creado)
+      expect(interception.response.statusCode).to.not.eq(201)
+       })
+      registroPage.validarFormatoPassword()
+
+     
+      
 
       
   });
-  });
+});
+  
 
 // Verificar que se muestre mensaje de error si el usuario ya está registrado con el mismo correo
-   it('Validación de usuario ya registrado con el mismo correo', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.correoYaRegistrado;
-      
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
-
+   it('Restricción de registro con correo ya existente', () => {
+     registroPage.completarDatosGenerales(data.correoYaRegistrado);
       cy.get(registroPage.buttonEnviar).click();
       // método  para evaluar el mensaje de error de usuario ya registrado con el mismo correo
       registroPage.validarCorreo();
 
       
   });
-  });
+ 
 
 
   
 // Verificar que se muestre mensaje de error si el usuario ya hay un usuario registrado con el mismo DNI
-   it('Validación de usuario ya existente con el mismo DNI', () => {
-    cy.fixture('userRegister').then((data) => {
-      const user = data.dniYaRegistrado;
-      
-      registroPage.escribirNombre(user.nombres);
-      registroPage.escribirApellido(user.apellido);
-      registroPage.escribirTelefono(user.telefono);
-      registroPage.escribirDNI(user.dni);
-      registroPage.seleccionarProvincia(user.provincia);
-      registroPage.seleccionarLocalidad(user.localidad);
-      registroPage.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
-      registroPage.escribirCorreo(user.email);
-      registroPage.confirmarCorreo(user.confirmarEmail);
-      registroPage.escribirPassword(user.password);
-      registroPage.confirmarPassword(user.repetirPassword);
-
+   it('Restricción de registro con DNI ya existente ', () => {
+     registroPage.completarDatosGenerales(data.dniYaRegistrado);
       cy.get(registroPage.buttonEnviar).click();
       // método  para evaluar el mensaje de error de usuario ya registrado con el mismo DNI
       registroPage.validarDNI();
 
       
   });
-  });
 
-  // Validar redirección a la vista del login
-   it('Validación redirección al login', () => {
+
+  // Verificar que la plataforma no permita el registro de menores de 18 años 
+
+  it('Validar restricción de registro a menores de 18 años ', () => {
+
+     // cargo la data de usuario menor de edad  que esta en fixture
+  cy.fixture('userRegister').then((data) => {
+    const baseUser = data.noRegistrarMenoresDe18;
+
+    // sobrescribo lo que necesito aleatorio
+    const userConDatosAleatorios = {
+      ...baseUser,
+      email: generarCorreo(),
+      confirmarEmail: generarCorreo(),
+      dni: generarDNI()
+    };
+
+    // uso el método para llenar los datos generales 
+    registroPage.completarDatosGenerales(userConDatosAleatorios);
+
+    // interceptar la petición de registro
+    cy.intercept('POST','**/api/backend/register/register-user').as('register');
+
+    cy.get(registroPage.buttonEnviar).click();
+
+    // validar que el usuario no se pueda registrar porque es menor de edad 
+    cy.wait('@register').then((interception) => {
+      expect(interception.response.statusCode, "El usuario NO debería registrarse siendo menor de edad")
+  .to.not.eq(201);
+    });
+  });
+});
+
+
+
+  
+
+  // Validar redirección a la vista del login, si el usuario elije la opción de que ya tiene una cuenta 
+   it('Validar redirección al login', () => {
     
       cy.get(registroPage.btnLoginLink).click();
       cy.url().should('include', 'https://ticketazo.com.ar/auth/login')
+     
 
       
   });
  
-
 });
+
+
--- cypress/support/pages/RegistroPage.js
+++ cypress/support/pages/RegistroPage.js
@@ -44,11 +44,11 @@ class RegistroPage {
   }
 
   escribirPassword(password){
-    if (password) cy.get(this.inputPassword).type(password);
+    if (password) cy.get(this.inputPassword).clear().type(password);
   }
 
   confirmarPassword(password){
-   if (password) cy.get(this.inputConfPass).type(password);
+   if (password) cy.get(this.inputConfPass).clear().type(password);
   }
 
    escribirCorreo(correo){
@@ -88,7 +88,7 @@ class RegistroPage {
     }
   }
 
-  //Método para validar ue hayan  mensajes de error y se muestre el texto "Completa este campo"
+  //Método para validar que hayan  mensajes de error y se muestre el texto "Completa este campo"
 
 
   validarError() {
@@ -139,12 +139,49 @@ validarErrorEmail (correo) {
 
 }
 
+// Método para completar datos generales (todo el formulario)
+completarDatosGenerales(user) {
+  this.escribirNombre(user.nombres);
+  this.escribirApellido(user.apellido);
+  this.escribirTelefono(user.telefono);
+  this.escribirDNI(user.dni);
+  this.seleccionarProvincia(user.provincia);
+  this.seleccionarLocalidad(user.localidad);
+  this.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
+  this.escribirCorreo(user.email);
+  this.confirmarCorreo(user.confirmarEmail);
+  this.escribirPassword(user.password);
+  this.confirmarPassword(user.repetirPassword);
+}
+
+
+// Método para completar datos generales menos contraseña
+completarDatosSinPassword(user) {
+  this.escribirNombre(user.nombres);
+  this.escribirApellido(user.apellido);
+  this.escribirTelefono(user.telefono);
+  this.escribirDNI(user.dni);
+  this.seleccionarProvincia(user.provincia);
+  this.seleccionarLocalidad(user.localidad);
+  this.ingresarFechaNacimiento(user.dia, user.mes, user.anio);
+  this.escribirCorreo(user.email);
+  this.confirmarCorreo(user.confirmarEmail);
+}
+
+
+
+//Método para validar el mensaje de error sobre un correo electrónico no válido 
+validarDatoInvalido(){
+  cy.get(this.dataError)
+  .should('be.visible')
+  .and('contain.text', 'El correo electrónico no es válido');
+}
 
 // Método para validar el mensaje de error sobre correos electrónicos que no coinciden 
 validarCoincidenciaEmail(){
   cy.get(this.dataError)
   .should('be.visible')
-  .and('have.text', 'Los correos electrónicos no coinciden');
+  .and('contain.text', 'Los correos electrónicos no coinciden');
   
 }
 
@@ -152,7 +189,7 @@ validarCoincidenciaEmail(){
 validarCoincidenciaPassword(){
   cy.get(this.dataError)
   .should('be.visible')
-  .and('have.text', 'Las contraseñas no coinciden');
+  .and('contain.text', 'Las contraseñas no coinciden');
   
 }
 
@@ -161,7 +198,7 @@ validarCoincidenciaPassword(){
 validarFormatoPassword(){
   cy.get(this.dataError)
   .should('be.visible')
-  .and('have.text', 'La contraseña debe tener al menos 8 caracteres, incluyendo mayúsculas, minúsculas, números y símbolos.');
+  .and('contain.text', 'La contraseña debe tener al menos 8 caracteres, incluyendo mayúsculas, minúsculas, números y símbolos.');
   
 }
 
@@ -170,7 +207,7 @@ validarFormatoPassword(){
 validarDNI(){
   cy.get(this.dataError)
   .should('be.visible')
-  .and('have.text', 'Ya existe un usuario registrado con ese DNI');
+  .and('contain.text', 'Ya existe un usuario registrado con ese DNI');
   
 }
 
@@ -179,7 +216,7 @@ validarDNI(){
 validarCorreo(){
   cy.get(this.dataError)
   .should('be.visible')
-  .and('have.text', 'Ya existe un usuario registrado con ese correo electrónico');
+  .and('contain.text', 'Ya existe un usuario registrado con ese correo electrónico');
   
 }
 }
